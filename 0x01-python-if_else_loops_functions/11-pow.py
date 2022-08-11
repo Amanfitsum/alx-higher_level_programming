a8cfa06593c34bf62b3352260cc9425fd785e110
Amanuel Gebreyesus <amanuealfitsum1@gmail.com>
2022-08-11 07:48:49 -0700

    my commit message

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Amanuel Fitsum
+
+def pow(a, b):
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Amanuel Gebreyesus
+
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
